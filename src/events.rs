eacfd6590d2be388f4981bce8b87bf10883df49e
ninefox <unvariant.winter@gmail.com>
2022-06-15 15:06:50 -0700

    player respawning

--- src/events.rs
+++ src/events.rs
@@ -3,6 +3,8 @@ use bevy_ecs_tilemap::prelude::*;
 
 use crate::triggers::{ButtonRes, DoorRes, Button};
 use crate::collide::{GameEvent};
+use crate::tiled_loader::WorldObject;
+use crate::map::spawn_map;
 
 pub struct EventPlugin;
 
@@ -12,18 +14,26 @@ impl Plugin for EventPlugin {
     }
 }
 
-fn handle_events(mut commands: Commands, mut events: EventReader<GameEvent>, mut buttons: ResMut<ButtonRes>, mut doors: ResMut<DoorRes>, mut map_query: MapQuery) {
+fn handle_events(mut commands: Commands, mut events: EventReader<GameEvent>, mut buttons: ResMut<ButtonRes>, mut doors: ResMut<DoorRes>, mut map_query: MapQuery, mut entities: Query<Entity, With<WorldObject>>, asset_server: Res<AssetServer>) {
     let mut is_dead = false;
     for event in events.iter() {
         match event {
             GameEvent::Death => is_dead = true,
             GameEvent::Sensor(id) => {
-
+                if let Some(button) = buttons.0.get_mut(&id) {
+                    let door = doors.0.get_mut(&button.door).unwrap();
+                    button.toggle();
+                    *door -= button.is_pressed() as usize;
+                }
             },
         }
     }
 
     if is_dead {
         map_query.despawn(&mut commands, 0);
+        for entity in entities.iter() {
+            commands.entity(entity).despawn();
+        }
+        spawn_map(commands, asset_server);
     }
 }
