0bfc91ea6ee54a79481b1177673e63256e152d22
ninefox <unvariant.winter@gmail.com>
2022-06-15 14:31:40 -0700

    player death

--- src/events.rs
+++ src/events.rs
@@ -4,7 +4,7 @@ use bevy_ecs_tilemap::prelude::*;
 use crate::triggers::{ButtonRes, DoorRes, Button};
 use crate::collide::{GameEvent};
 
-fn handle_events(mut events: EventReader<GameEvent>, mut buttons: ResMut<ButtonRes>, mut doors: ResMut<DoorRes>, mut map_query: MapQuery) {
+fn handle_events(mut commands: Commands, mut events: EventReader<GameEvent>, mut buttons: ResMut<ButtonRes>, mut doors: ResMut<DoorRes>, mut map_query: MapQuery) {
     let mut is_dead = false;
     for event in events.iter() {
         match event {
@@ -14,21 +14,8 @@ fn handle_events(mut events: EventReader<GameEvent>, mut buttons: ResMut<ButtonR
             },
         }
     }
-}
 
-/*
-fn death_update_map(
-    mut player_events: EventReader<GamerEvent>,
-    mut commands: Commands,
-
-) {
-    if player_events
-        .iter()
-        .filter(|e| **e == PlayerEvent::Death)
-        .count()
-        > 0
-    {
-        map_query.despawn(&mut commands, 0u16);
+    if is_dead {
+        map_query.despawn(&mut commands, 0);
     }
 }
-*/
